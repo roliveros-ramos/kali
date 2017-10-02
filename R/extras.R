26748a98043212e0867d404563f05bf6fb59ffd2
roliveros-ramos <ricardo.oliveros@gmail.com>
2017-10-02 13:45:41 +0200

    bug in getTime

--- R/extras.R
+++ R/extras.R
@@ -1,9 +1,9 @@
 getTime = function(data) {
   names(data) = tolower(names(data))
   if(all(c("year", "month", "day") %in% names(data)))
-    return(with(env, round(year + (month-1)/12 + (day-1)/365, 5)))
+    return(with(data, round(year + (month-1)/12 + (day-1)/365, 5)))
   if(all(c("year", "month") %in% names(data)))
-    return(with(env, round(year + (month-1)/12 + (15-1)/365, 5)))
+    return(with(data, round(year + (month-1)/12 + (15-1)/365, 5)))
   if("date" %in% names(data)) {
     dates = ymd(data$date)
     year  = year(dates)
