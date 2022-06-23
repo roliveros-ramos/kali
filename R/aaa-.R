a8d92b446a4a3c26b0b25290ef78a20ef7be2782
Ricardo Oliveros-Ramos <ricardo.oliveros@gmail.com>
2022-06-23 21:18:31 +0200

    Improving Imports.

--- R/aaa-.R
+++ R/aaa-.R
@@ -1,4 +1,33 @@
 #' @import methods
 NULL
+#' @import graphics
+NULL
+#' @importFrom stats predict complete.cases density
+#' setNames na.omit median quantile splinefun
+#' AIC BIC approx as.formula binomial
+#' coef formula lm runif sd weighted.mean
+NULL
+#' @importFrom maps map
+NULL
+#' @importFrom utils as.relistable read.csv write.csv
+#' relist head tail txtProgressBar setTxtProgressBar combn
+NULL
+#' @importFrom grDevices chull rainbow col2rgb dev.copy dev.off rgb xy.coords
+NULL
 #' @importMethodsFrom sp plot
 NULL
+#' @importFrom sp SpatialPoints over
+NULL
+#' @importFrom fields poly.image image.plot interp.surface.grid tim.colors
+#' imageplot.info imageplot.setup
+NULL
+#' @importFrom akima interp  interpp
+NULL
+#' @importFrom lubridate yday<- year month ymd leap_year
+NULL
+#' @importFrom maptools map2SpatialPolygons
+NULL
+
+
+
+
